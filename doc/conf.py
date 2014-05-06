a65c206190fe1a73d373f39f2b528897a00512c7
Esmail Fadae <efadae@hotmail.com>
2014-05-06 13:32:36 -0400

    Working out the kinks with Sphinx.

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # StrongBox documentation build configuration file, created by
-# sphinx-quickstart on Tue May  6 13:15:47 2014.
+# sphinx-quickstart on Tue May  6 13:29:52 2014.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -28,7 +28,9 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
--- doc/index.rst
+++ doc/index.rst
@@ -1,5 +1,5 @@
 .. StrongBox documentation master file, created by
-   sphinx-quickstart on Tue May  6 13:15:47 2014.
+   sphinx-quickstart on Tue May  6 13:29:52 2014.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
@@ -10,6 +10,12 @@ Contents:
 
 .. toctree::
    :maxdepth: 2
+
+.. automodule:: StrongBox
+   :members:
+
+.. automodule:: DirectoryMerkelTree
+   :members:
 
 
 
