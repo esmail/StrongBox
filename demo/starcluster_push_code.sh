cf612775eb608ac72f87e408304e012913e2b744
Esmail Fadae <efadae@hotmail.com>
2014-06-04 13:00:33 -0400

    Updated source file names.

--- demo/starcluster_push_code.sh
+++ demo/starcluster_push_code.sh
@@ -2,6 +2,6 @@
 
 # Tested with StarCluster 0.95.5
 
-starcluster put strongbox_cluster --node master ../StrongBox.py ../DirectoryMerkelTree.py /root
-starcluster put strongbox_cluster --node node001 ../StrongBox.py ../DirectoryMerkelTree.py /root
-starcluster put strongbox_cluster --node node002 ../StrongBox.py ../DirectoryMerkelTree.py /root
+starcluster put strongbox_cluster --node master ../StrongBox.py ../DirectoryMerkleTree.py /root
+starcluster put strongbox_cluster --node node001 ../StrongBox.py ../DirectoryMerkleTree.py /root
+starcluster put strongbox_cluster --node node002 ../StrongBox.py ../DirectoryMerkleTree.py /root
